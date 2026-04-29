cd0a3b4d28420646c948a9695b076497b11de914
Emmanuel Lopez Guerrero <manelpz@icloud.com>
2026-04-28 20:32:25 -0600

    update

--- src/main/java/com/emma/controllers/mainController.java
+++ src/main/java/com/emma/controllers/mainController.java
@@ -1,9 +1,50 @@
 
 package com.emma.controllers;
 
+import com.emma.models.Book;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class mainController {
+
+
+
+    @FXML 
+    private TextField tfId;
+
+    @FXML 
+    private TextField tfTitle;
+
+    @FXML 
+    private TextField tfAuthor;
+
+    @FXML 
+    private TextField tfYear;
+
+    @FXML 
+    private TextField tfPages;
+
+    @FXML 
+    private TableView<Book> tbBooks;
+
+    @FXML 
+    private TableColumn<Book, Integer> colId;
+
+    @FXML 
+    private TableColumn<Book, String> colTitle;
+
+    @FXML 
+    private TableColumn<Book, String> colAuthor;
+
+    @FXML 
+    private TableColumn<Book, Integer> colYear;
+
+    @FXML 
+    private TableColumn<Book, Integer> colPages;
+    
 
      @FXML
     private void handleBtnAction(javafx.event.ActionEvent event) {
